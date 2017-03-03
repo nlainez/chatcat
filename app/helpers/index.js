a1a4d50c756ae17094d2e2d221545e9b57336194
nlainez <norman.joya@gmail.com>
2017-03-02 18:48:42 -0600

    updating code
    adding the ability to interact messages with some other users

--- app/helpers/index.js
+++ app/helpers/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const router = require('express').Router();
 const db = require('../db');
+const crypto = require('crypto');
 
 // iterate though the routes object and mount routes
 let _registerRoutes = (routes, method) => {
@@ -73,10 +74,102 @@ let isAuthenticated = (req, res, next) => {
   }
 };
 
+// find a chatroom by a given name
+let findRoomByName = (allrooms, room) => {
+  let findRoom = allrooms.findIndex((element, index, array) => {
+    if (element.room === room){
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  return findRoom > -1 ? true : false;
+};
+
+// generate a unique roomID
+let randomHex = () => {
+  return crypto.randomBytes(24).toString('hex');
+};
+
+// find a chatroom with a given id
+let findRoomById = (allrooms, roomID) => {
+  return allrooms.find((element, index, array) => {
+    if(element.roomID === roomID) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+};
+// add a user to chat
+let addUserToRoom = (allrooms, data, socket) => {
+  // get the room object
+  let getRoom = findRoomById(allrooms, data.roomID);
+  if(getRoom !== undefined) {
+    // get the active user's ID (objectID as used in session)
+    let userID = socket.request.session.passport.user;
+
+    // check to see if this user already exists in the chatroom
+    let checkUser = getRoom.users.findIndex((element, index, array) => {
+      if(element.userID === userID) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    // if the user is already present in the room, remove him first
+    if(checkUser > -1) {
+      getRoom.users.splice(checkUser, 1);
+    }
+
+    // push the user into the room's users array
+    getRoom.users.push({
+      socketID: socket.id,
+      userID,
+      user: data.user,
+      userPic: data.userPic
+    });
+
+    // join the room channel
+    socket.join(data.roomID);
+
+    // return the updated room object
+    return getRoom;
+  }
+};
+
+// find and purge the user when a socket disconnects
+let removeUserFromRoom = (allrooms, socket) => {
+  for (let room of allrooms) {
+    let findUser = room.users.findIndex((element, index, array) => {
+      if(element.socketID === socket.id){
+        return true;
+      } else {
+        return false;
+      }
+      // return element.socketID === socket.id ? true : false;
+    });
+
+    if(findUser > -1) {
+      socket.leave(room.roomID);
+      room.users.splice(findUser, 1);
+
+      return room;
+    }
+  }
+};
+
 module.exports = {
   route,
   findOne,
   createNewUser,
   findById,
-  isAuthenticated
+  isAuthenticated,
+  findRoomByName,
+  randomHex,
+  findRoomById,
+  addUserToRoom,
+  removeUserFromRoom
 };
--- app/socket/index.js
+++ app/socket/index.js
@@ -1,9 +1,66 @@
 'use strict';
+const h = require('../helpers');
 
 module.exports = (io, app) => {
   let allrooms = app.locals.chatrooms;
 
+  // allrooms.push({
+  //   room: 'Good food',
+  //   roomID: '0001',
+  //   users: []
+  // });
+
+  // allrooms.push({
+  //   room: 'Cloud computing',
+  //   roomID: '0002',
+  //   users: []
+  // });
+
   io.of('/roomslist').on('connection', socket => {
-    console.log('Socket.io connected to client!');
+    socket.on('getChatrooms', () => {
+      socket.emit('chatRoomsList', JSON.stringify(allrooms));
+    });
+
+    socket.on('createNewRoom', newRoomInput => {
+      // console.log(newRoomInput);
+      // check to see if a room with the same title exists or not
+      // if not, create one and broadcast it to everyone
+      if(!h.findRoomByName(allrooms, newRoomInput)) {
+        allrooms.push({
+          room: newRoomInput,
+          roomID: h.randomHex(), //write a helper to automatically generate this id
+          users: []
+        });
+
+        // emit an updated list to the creator
+        socket.emit('chatRoomsList', JSON.stringify(allrooms));
+
+        // emit an updated list to everyone connected to the rooms page
+        socket.broadcast.emit('chatRoomsList', JSON.stringify(allrooms));
+      }
+    });
+  });
+
+  io.of('/chatter').on('connection', socket => {
+    // join a chatroom
+    socket.on('join', data => {
+      let usersList = h.addUserToRoom(allrooms, data, socket);
+
+      // update the list of active users as shown on the chatroom page
+      socket.broadcast.to(data.roomID).emit('updateUsersList', JSON.stringify(usersList.users));
+      socket.emit('updateUsersList', JSON.stringify(usersList.users));
+    });
+
+    // when a socket exits
+    socket.on('disconnect', () => {
+      // find the room, to wich the socket is connected to and purge the user
+      let room = h.removeUserFromRoom(allrooms, socket);
+      socket.broadcast.to(room.roomID).emit('updateUsersList', JSON.stringify(room.users));
+      socket.emit('updateUsersList', JSON.stringify(room.users));
+    });
+
+    socket.on('newMessage', data => {
+      socket.broadcast.to(data.roomID).emit('inMessage', JSON.stringify(data));
+    });
   });
 }
